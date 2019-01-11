966060ed703dc14ed6f267438d2350604e57fbd7
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-01-11 13:44:05 -0500

    NIM does not work PerfectInteger does indeed work

--- src/chapterSeven/Nim.java
+++ src/chapterSeven/Nim.java
@@ -3,43 +3,45 @@ package chapterSeven;
 import java.util.Scanner;
 
 public class Nim {	
-	static int stones =(int)( Math.random() * 15) + 15;
 	static Scanner input = new Scanner(System.in);
 
 	public static void main(String args[]) {
+		  int totalStones =(int)( Math.random() * 15) + 15;
 		
-		
-		while(stones > 0) {
+		while(totalStones > 0) {
 			
 			//boolean turn = false;// false = players turn, true = cpu turn
-			System.out.println("There are " + stones + "stones left. How many would you like?");
-			player();
+			System.out.println("There are " + totalStones + "stones left. How many would you like?");
+			player(totalStones);
+			totalStones = totalStones ;
+			System.out.println("There are " + totalStones + "stones left. How many would you like?");
+			cpu(totalStones);
 			
-			System.out.println("There are " + stones + "stones left. How many would you like?");
-			cpu();
-			
-		}//
+		}
 	}
-	
-	public static void drawStones() {
+	public static int player(int stones) {
+		//handles players turn
+		int takenStones = input.nextInt();
+		return takenStones;
+
+	}
+	public static void cpu(int stones) {
+		//handles computers turn
+
+
+	}
+	public static void drawStones( ) {
+		
 		//handles computer randoms
+		
 	}
 	
 	public static void isValidEntry() {
 		//checks to make sure between 1-3 stones 
 	}
 	
-	public static void player() {
-		//handles players turn
-		int takenStones = input.nextInt();
-		stones = stones - takenStones;
-
-	}
 	
-	public static void cpu() {
-		//handles computers turn
-
-
-	}
+	
+	
 	
 }
--- src/chapterSeven/PerfectInteger.java
+++ src/chapterSeven/PerfectInteger.java
@@ -5,11 +5,44 @@ import java.util.Scanner;
 public class PerfectInteger {
 
 	public static void main(String args[]) {
-		
+		/*
+		 * we need to take all integers up to 100 factor each number add each factor to
+		 * see if the sum is the same as the original number
+		 */
 		Scanner input = new Scanner(System.in);
-		
+		int sum = 0;
+
+		for (int x = 1; x <= 100; x++) {
+
+			for (int factor = 1; factor <= x; factor++) {
+
+				if (x % factor == 0) {
+					sum = sum + factor;
+					System.out.println("factor: " + factor);
+				}
+
+			}
+			
+			
+			
+			if (x > 1) {
+				sum = sum - x;
+			}
+			System.out.println("sum " + sum);
+			if (isPerfect(sum, x) == true) {
+				System.out.println(x + " is perfect");
+			
+				sum = 0;
+			}
+			System.out.print("\n");
+		}
+
 	}
-	
-	
-	
+
+	public static boolean isPerfect(int number, int sums) {
+		if (number == sums) {
+			return true;
+		} else
+			return false;
+	}
 }
