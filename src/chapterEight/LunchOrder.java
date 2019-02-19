2e400bd4e1f479f77cd73b2e3835d26abff8d249
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-02-19 13:46:00 -0500

    asfg

---
+++ src/chapterEight/LunchOrder.java
@@ -0,0 +1,46 @@
+package chapterEight;
+
+public class LunchOrder {
+private double price, fat, carb, fiber;
+public String name = new String();
+
+
+public LunchOrder(double price, double fat, double carb, double fiber, String name) {
+	
+}
+
+public double getCarb() {
+	return carb;
+}
+
+public void setCarb(double carb) {
+	this.carb = carb;
+}
+
+public double getFiber() {
+	return fiber;
+}
+
+public void setFiber(double fiber) {
+	this.fiber = fiber;
+}
+
+public double getFat() {
+	return fat;
+}
+
+public void setFat(double fat) {
+	this.fat = fat;
+}
+
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
+
+
+}
