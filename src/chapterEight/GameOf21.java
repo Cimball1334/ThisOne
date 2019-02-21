c014a77fac6c46164bb83cd94083f34e3c6b19f0
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-02-21 13:46:01 -0500

    need to double check game of 21 if "hit enter is good"

--- src/chapterEight/GameOf21.java
+++ src/chapterEight/GameOf21.java
@@ -13,6 +13,29 @@ public class GameOf21 {
 		setTotal(0);
 	
 	}
+	public void play(Object c) {
+	GameOf21 test = (GameOf21) c;
+	test.setTotal(0);
+	
+		 int cpu = (int)(Math.random() * 19 + 2);
+			test.giveCards();
+			
+			if(input.nextInt() == 1) {
+				test.drawCard();
+			}
+			System.out.println("The CPU has " + cpu);
+			if(test.getTotal() > 21 ) {
+				System.out.println("You Lost!!");
+			}else if( test.getTotal() > cpu) {
+				System.out.println("You Won!!");
+			}else {
+				System.out.println("You Lost :'( ");
+			
+			}
+			System.out.println("Hit enter to play again, Type stop to Stop playing");
+	}
+			
+	
 	public void giveCards() {
 		//give them the two cards off the start
 				int card = (int)(Math.random() * 12 + 1);
@@ -65,22 +88,24 @@ public class GameOf21 {
 		int card = (int)(Math.random() * 12 + 1);
 		
 		if(card == 1){
-			System.out.println("You pulled an Ace, do you want it to be 1 or 11?");
+			System.out.println("You have an Ace, do you want it to be 1 or 11?");
 			if(input.nextInt() == 1) {
 				setTotal(total + 1);
-				
 			}else {
 				setTotal(total + 11);
-				
 			}
 		}
 		else if(card < 11) {
 			setTotal(total + card);
-			System.out.println("You pulled a " + card);
 		}else {
 			setTotal(total + 10);
-			System.out.println("You pulled a " + card);
+		
 		}
+		if(card < 11) {
+			System.out.println("You pulled a: " + card);
+			}else {
+				System.out.println("You pulled a face card, you have 10");
+			}
 		System.out.println("You have: " + total);
 	
 	}
--- src/chapterEight/Play21.java
+++ src/chapterEight/Play21.java
@@ -7,23 +7,15 @@ public class Play21 {
 	public static void main(String args[]) {
 		
 	 Scanner input = new Scanner(System.in);
+	String stop = new String("stop") ;
+	
 	 GameOf21 test = new GameOf21();
+	 do {
+	
+	 test.play(test);
+	 }	
+	 while(input.next().equals(stop) != true);
 	 
-	 int cpu = (int)(Math.random() * 19 + 2);
-		test.giveCards();
-		
-		if(input.nextInt() == 1) {
-			test.drawCard();
-		}
-		System.out.println("The CPU has " + cpu);
-		if(test.getTotal() > 21 ) {
-			System.out.println("You Lost!!");
-		}else if( test.getTotal() > cpu) {
-			System.out.println("You Won!!");
-		}else {
-			System.out.println("You Lost :'( ");
-		}
-		
 	}
 	
 }
