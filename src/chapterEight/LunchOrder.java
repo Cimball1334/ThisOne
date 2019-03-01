e9426f396ec276c9add516bd41b6decc96e6516c
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-01 12:57:16 -0500

    tadah

--- src/chapterEight/LunchOrder.java
+++ src/chapterEight/LunchOrder.java
@@ -5,8 +5,8 @@ public class LunchOrder {
 	Food Hamburger = new Food(1.85, 9, 33, 1, "hamburger");
 	
 	public static void Main(String args[]) {
-		
-		System.out.println("Iteam");
+		System.out.println("test");
+		System.out.println("Iteam \t Fat(g) \t Carbohydrates(g) \t Fiber(g)");
 		
 	}
 	
