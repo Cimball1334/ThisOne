10e150ac417a5fd669b35d306e0d5b8126cbc9f3
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-07 13:21:46 -0500

    read the comment underneath game.answer in AdderGameRunner

--- src/chapterEight/Adder.java
+++ src/chapterEight/Adder.java
@@ -6,19 +6,85 @@ public class Adder{
 		
 	Scanner input = new Scanner(System.in);
 	
+	private int firstNumber, secondNumber, score, correctAnswer, trial;
+	
 	public Adder() {
 		//constructor
+		newGame();
+		
+	}
+	
+	public void newGame() {
+		setFirstNumber(0);
+		setSecondNumber(0);
+		setCorrectAnswer(0);
+		setScore(0);
+	}
+	
+	public void newQuestion() {
+		setFirstNumber(0);
+		setSecondNumber(0);
+		setCorrectAnswer(0);
+	}
+	
+	
+	public void question() {
+		//asks the questions
+		setFirstNumber( (int)(Math.random() * 20) );
+		setSecondNumber( (int)(Math.random() * 20) );
+
+		System.out.println(getFirstNumber() + " + " + getSecondNumber() + " = ");
+		
+	}
+	
+	public void answer(int inputedAnswer) {
+		setCorrectAnswer( getFirstNumber() + getSecondNumber() );
+		
+		
+		
+		if(inputedAnswer == getCorrectAnswer() ) {
+			question();
+		} 
+			
 		
 		
 	}
 	
-	public String question() {
-		//asks the questions
-		
+	
 
-		
-		
-		return null;
+	public int getFirstNumber() {
+		return firstNumber;
+	}
+	
+
+	public void setFirstNumber(int firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public int getSecondNumber() {
+		return secondNumber;
+	}
+
+	public void setSecondNumber(int secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+	
+
+	public int getScore() {
+		return score;
+	}
+	
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getCorrectAnswer() {
+		return correctAnswer;
+	}
+
+	public void setCorrectAnswer(int correctAnswer) {
+		this.correctAnswer = correctAnswer;
 	}	
 	
 	
--- src/chapterEight/AdderGameRunner.java
+++ src/chapterEight/AdderGameRunner.java
@@ -1,5 +1,22 @@
 package chapterEight;
 
+import java.util.Scanner;
+
 public class AdderGameRunner {
 
+	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		int answer = 0;
+	
+		Adder game = new Adder();
+		
+		game.question();
+		game.answer(input.nextInt());
+
+		//make this loop whenever i can
+		
+		
+	}
+	
 }
