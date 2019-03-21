47fecd3e364ccf8596b69c073649db7f3abdc37e
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-21 13:01:53 -0400

    tyu

--- src/ChapterTen/EvensAndOdds.java
+++ src/ChapterTen/EvensAndOdds.java
@@ -9,7 +9,7 @@ public class EvensAndOdds {
 		
 		int evennumber = 0, oddnumber = 0;
 		
-		for(int x = 0; x<26; x++) {
+		for(int x = 0; x<25; x++) {
 			all[x] = (int)(Math.random() * 100 );
 			
 			if(all[x] % 2 == 0 ) {
@@ -18,11 +18,10 @@ public class EvensAndOdds {
 				oddnumber++;
 			}
 		}
-		
+	
 		int[] evens = new int[evennumber], odds = new int[oddnumber];
 		
-		for(int x = 0; x<26; x++) {
-		
+		for(int x = 0; x<25; x++) {
 			if(all[x] % 2 == 0 ) {
 				evens[x] = all[x];
 			}else {
@@ -30,14 +29,14 @@ public class EvensAndOdds {
 			}
 		}
 		
-		for(int x = 0; x<26; x++) {
+		for(int x = 0; x<25; x++) {
 		
 			System.out.println("Odds: ");
+	
 			System.out.print(odds[x]);
 		
 		}
 		
 		}
 		
-	}
-}	
+}
