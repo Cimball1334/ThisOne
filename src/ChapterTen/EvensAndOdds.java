f76af0d5d29963dc3ecef8deb6170919abd1c303
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-21 12:58:17 -0400

    maybe, this is broken i think

--- src/ChapterTen/EvensAndOdds.java
+++ src/ChapterTen/EvensAndOdds.java
@@ -4,35 +4,40 @@ public class EvensAndOdds {
 
 	public static void main(String args[]) {
 		
-		int[] odds = new int[25];
-		int[] evens = new int[25];
-		
-		int number = 0, spot;
+		int[] all = new int[25];
 		
 		
-		for(spot = 0; spot < 25; spot ++) {
+		int evennumber = 0, oddnumber = 0;
 		
-			number = (int)(Math.random() * 100);
+		for(int x = 0; x<26; x++) {
+			all[x] = (int)(Math.random() * 100 );
 			
-			if( number % 2 == 0) {
-			
-				evens[spot] = number;
-
+			if(all[x] % 2 == 0 ) {
+				evennumber++;
 			}else {
-				
-				odds[spot] = number;
-				
+				oddnumber++;
 			}
+		}
 		
-			
+		int[] evens = new int[evennumber], odds = new int[oddnumber];
+		
+		for(int x = 0; x<26; x++) {
+		
+			if(all[x] % 2 == 0 ) {
+				evens[x] = all[x];
+			}else {
+				odds[x] = all[x];
+			}
+		}
+		
+		for(int x = 0; x<26; x++) {
+		
+			System.out.println("Odds: ");
+			System.out.print(odds[x]);
 		
 		}
 		
+		}
 		
-		
-		System.out.println("Odds: " + odds);
-		System.out.println("Evens: " + evens);
-
 	}
-	
-}
+}	
