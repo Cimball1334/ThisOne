045b60e7823689414416d7b0b165f200e517df66
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-25 13:46:08 -0400

    awrg

--- src/ChapterTen/Coder.java
+++ src/ChapterTen/Coder.java
@@ -1,5 +1,35 @@
 package ChapterTen;
 
+import java.util.Scanner;
+
 public class Coder {
 
+	  public static void main(String[] args)
+	    {
+	        Scanner input = new Scanner(System.in);
+
+	        String sentence = "";
+	        String code = "";
+	        
+	        System.out.println("Enter Text ");
+	        sentence = input.nextLine();
+	        sentence = sentence.toLowerCase();
+	
+	        for(int x = 0; x < sentence.length(); x++) {
+	        	int letter = (int)(sentence.charAt(x));
+	        	int coded = letter + 3;
+//	        	if(coded > 90) {
+//	        		coded -= 65;
+//	        	}
+	        	char codes = (char) coded;
+	        	//going from numbner to letter:::::
+	        	//cast as (char)
+	        	
+	        	code += codes;
+	        	
+	        }
+	        
+	        System.out.println(code);
+	        
+	    }
 }
