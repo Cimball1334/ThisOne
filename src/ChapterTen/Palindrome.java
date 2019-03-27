cca1ad28bf912a2b024451d6471321273f8cc29b
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-03-27 13:40:18 -0400

    palindrom no worky

--- src/ChapterTen/Palindrome.java
+++ src/ChapterTen/Palindrome.java
@@ -11,19 +11,23 @@ public static void main(String[] args)
 	System.out.println("Enter a thing:");
 	
 	String thing = new String(input.next());
-    String reverse = "";
-    
-    
-    for(int i = thing.length() - 1; i >= 0; i--)
-    {
-        reverse = reverse + thing.charAt(i);
-    }
-    
-    if(thing.equalsIgnoreCase(reverse)) {
-    	System.out.println("this is a palindrome");
-    }
+	//find first number and find last number
+	//x is starting ;;;;;  length - x is last
+	for(int x =0; x <= thing.length(); x++){
+		if(thing.charAt(x) != thing.charAt(thing.length() - 1 - x)) {
+			System.out.println("This is not a Palindrome");
+			x = 200000;
+		}
+	}
+	System.out.println("This is a palindrome");
+	//discover if they are the same dec ascii 
 	
+	//if they are continue to the next one subractnig the number we are on (now 2) from the final number to get the second to last number
 	
+	//diddo 
 	
+	// i want a fucking taco
+	
+	// done;
 }
 }
